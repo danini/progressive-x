dda2c323dd40b2c8a9af5414374924b4d93575ed
danini <majti89@gmail.com>
2019-08-21 16:13:03 +0200

    Creating the Progressive-X class.

---
+++ include/progressive_x.h
@@ -0,0 +1,169 @@
+#pragma once
+
+#include <iostream>
+#include <math.h>
+#include <random>
+#include <vector>
+
+#include "GCRANSAC.h"
+#include "types.h"
+
+#include "uniform_sampler.h"
+#include "prosac_sampler.h"
+#include "progressive_napsac_sampler.h"
+#include "fundamental_estimator.h"
+#include "homography_estimator.h"
+#include "essential_estimator.h"
+
+#include "solver_fundamental_matrix_seven_point.h"
+#include "solver_fundamental_matrix_eight_point.h"
+#include "solver_homography_four_point.h"
+#include "solver_essential_matrix_five_point_stewenius.h"
+
+struct MultiModelSettings
+{
+	bool do_final_iterated_least_squares, // Flag to decide a final iterated least-squares fitting is needed to polish the output model parameters.
+		do_local_optimization, // Flag to decide if local optimization is needed
+		do_graph_cut, // Flag to decide of graph-cut is used in the local optimization
+		use_inlier_limit; // Flag to decide if an inlier limit is used in the local optimization to speed up the procedure
+	
+	size_t cell_number_in_neighborhood_graph,
+		max_local_optimization_number, // Maximum number of local optimizations
+		min_iteration_number_before_lo, // Minimum number of RANSAC iterations before applying local optimization
+		min_ransac_iteration_number, // Minimum number of RANSAC iterations
+		max_ransac_iteration_number, // Maximum number of RANSAC iterations
+		max_unsuccessful_model_generations, // Maximum number of unsuccessful model generations
+		max_least_squares_iterations, // Maximum number of iterated least-squares iterations
+		max_graph_cut_number, // Maximum number of graph-cuts applied in each current_iteration
+		core_number; // Number of parallel threads
+
+	double confidence, // Required confidence in the result
+		neighborhood_sphere_radius, // The radius of the ball used for creating the neighborhood graph
+		inlier_outlier_threshold, // The inlier-outlier threshold
+		spatial_coherence_weight; // The weight of the spatial coherence term
+
+	MultiModelSettings() :
+		do_final_iterated_least_squares(true),
+		do_local_optimization(true),
+		do_graph_cut(true),
+		use_inlier_limit(false),
+		cell_number_in_neighborhood_graph(8),
+		max_local_optimization_number(20),
+		max_graph_cut_number(std::numeric_limits<size_t>::max()),
+		max_least_squares_iterations(20),
+		min_iteration_number_before_lo(20),
+		min_ransac_iteration_number(20),
+		neighborhood_sphere_radius(20),
+		max_ransac_iteration_number(std::numeric_limits<size_t>::max()),
+		max_unsuccessful_model_generations(100),
+		core_number(1),
+		spatial_coherence_weight(0.14),
+		inlier_outlier_threshold(2.0),
+		confidence(0.95)
+	{
+
+	}
+};
+
+struct MultiModelStatistics
+{
+	double processing_time;
+	std::vector<std::vector<size_t>> inliers_of_each_model;
+};
+
+template<class _NeighborhoodGraph, 
+	class _ModelEstimator, 
+	class _MainSampler, 
+	class _LocalOptimizerSampler>
+class ProgressiveX
+{
+protected:
+	std::unique_ptr<GCRANSAC<_ModelEstimator, _NeighborhoodGraph>> proposal_engine;
+	_ModelEstimator model_estimator;
+	MultiModelStatistics statistics;
+	std::vector<Model> models;
+	std::vector<double> compound_preference_vector;
+	double compound_preference_vector_sum,
+		compound_preference_vector_length;
+
+	void initialize(const cv::Mat &data_);
+
+public:
+	MultiModelSettings settings;
+
+	ProgressiveX() 
+	{
+	}
+
+	void run(const cv::Mat &data_,
+		const _NeighborhoodGraph &neighborhood_graph_, // The initialized neighborhood graph
+		_MainSampler &main_sampler,
+		_LocalOptimizerSampler &local_optimization_sampler);
+
+	const MultiModelStatistics &getStatistics() 
+	{
+		return statistics;
+	}
+
+
+};
+
+template<class _NeighborhoodGraph,
+	class _ModelEstimator,
+	class _MainSampler,
+	class _LocalOptimizerSampler>
+void ProgressiveX<_NeighborhoodGraph, _ModelEstimator, _MainSampler, _LocalOptimizerSampler>::run(
+	const cv::Mat &data_,
+	const _NeighborhoodGraph &neighborhood_graph_, // The initialized neighborhood graph
+	_MainSampler &main_sampler_,
+	_LocalOptimizerSampler &local_optimization_sampler_)
+{
+	// Initializing the procedure
+	initialize(data_);
+
+	for (size_t current_iteration = 0; current_iteration < 100; ++current_iteration)
+	{
+		Model model;
+
+		/**************************
+		*** Model proposal step ***
+		**************************/
+		if (current_iteration > 0)
+			proposal_engine->setCompoundModel(&models,
+				&compound_preference_vector, 
+				compound_preference_vector_sum, 
+				compound_preference_vector_length);
+
+		proposal_engine->run(data_,
+			model_estimator,
+			&main_sampler_,
+			&local_optimization_sampler_,
+			&neighborhood_graph_,
+			model);
+		
+		statistics.inliers_of_each_model.emplace_back(proposal_engine->getRansacStatistics().inliers);
+		break;
+
+	}
+}
+
+template<class _NeighborhoodGraph,
+	class _ModelEstimator,
+	class _MainSampler,
+	class _LocalOptimizerSampler>
+void ProgressiveX<_NeighborhoodGraph, _ModelEstimator, _MainSampler, _LocalOptimizerSampler>::initialize(const cv::Mat &data_)
+{
+	// 
+
+	// Initializing the proposal engine, i.e., Graph-Cut RANSAC
+	proposal_engine = std::make_unique<GCRANSAC<_ModelEstimator, _NeighborhoodGraph>>();
+	Settings &proposal_engine_settings = proposal_engine->settings;
+	proposal_engine_settings.threshold = settings.inlier_outlier_threshold; // The inlier-outlier threshold
+	proposal_engine_settings.spatial_coherence_weight = settings.spatial_coherence_weight; // The weight of the spatial coherence term
+	proposal_engine_settings.confidence = settings.confidence; // The required confidence in the results
+	proposal_engine_settings.max_local_optimization_number = settings.max_local_optimization_number; // The maximm number of local optimizations
+	proposal_engine_settings.max_iteration_number = settings.max_ransac_iteration_number; // The maximum number of iterations
+	proposal_engine_settings.min_iteration_number = settings.min_ransac_iteration_number; // The minimum number of iterations
+	proposal_engine_settings.neighborhood_sphere_radius = settings.cell_number_in_neighborhood_graph; // The radius of the neighborhood ball
+	proposal_engine_settings.core_number = settings.core_number; // The number of parallel processes
+}
---
+++ src/main.cpp
@@ -0,0 +1,238 @@
+#include <vector>
+#include <thread>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <Eigen/Eigen>
+
+#include "utils.h"
+#include "grid_neighborhood_graph.h"
+#include "uniform_sampler.h"
+#include "prosac_sampler.h"
+#include "progressive_napsac_sampler.h"
+#include "fundamental_estimator.h"
+#include "homography_estimator.h"
+#include "essential_estimator.h"
+
+#include "progressive_x.h"
+
+#include <ctime>
+#include <direct.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+struct stat info;
+
+void testMultiHomographyFitting(
+	const std::string &source_path_,
+	const std::string &destination_path_,
+	const std::string &out_correspondence_path_,
+	const std::string &in_correspondence_path_,
+	const std::string &output_match_image_path_,
+	const double confidence_,
+	const double inlier_outlier_threshold_,
+	const double spatial_coherence_weight_,
+	const size_t cell_number_in_neighborhood_graph_,
+	const int fps_);
+
+bool initializeScene(const std::string &scene_name_,
+	std::string &src_image_path_,
+	std::string &dst_image_path_,
+	std::string &input_correspondence_path_,
+	std::string &output_correspondence_path_,
+	std::string &output_matched_image_path_);
+
+int main(int argc, const char* argv[])
+{
+	std::string scene = "johnssona";
+
+	printf("Processed scene = '%s'\n", scene.c_str());
+	std::string src_image_path, // Path of the source image
+		dst_image_path, // Path of the destination image
+		input_correspondence_path, // Path where the detected correspondences are saved
+		output_correspondence_path, // Path where the inlier correspondences are saved
+		output_matched_image_path; // Path where the matched image is saved
+
+	// Initializing the paths
+	initializeScene(scene,
+		src_image_path,
+		dst_image_path,
+		input_correspondence_path,
+		output_correspondence_path,
+		output_matched_image_path);
+
+	testMultiHomographyFitting(
+		src_image_path, // The source image's path
+		dst_image_path, // The destination image's path
+		input_correspondence_path, // The path where the detected correspondences (before the robust estimation) will be saved (or loaded from if exists)
+		output_correspondence_path, // The path where the inliers of the estimated fundamental matrices will be saved
+		output_matched_image_path, // The path where the matched image pair will be saved
+		0.99, // The RANSAC confidence value
+		1.0, // The used inlier-outlier threshold in GC-RANSAC.
+		0.14, // The weight of the spatial coherence term in the graph-cut energy minimization.
+		8, // The radius of the neighborhood ball for determining the neighborhoods.
+		-1); // The required FPS limit. If it is set to -1, the algorithm will not be interrupted before finishing.
+
+	return 0;
+}
+
+std::vector<std::string> getAvailableTestScenes()
+{
+	return { "johnssona" };
+}
+
+bool initializeScene(const std::string &scene_name_,
+	std::string &src_image_path_,
+	std::string &dst_image_path_,
+	std::string &input_correspondence_path_,
+	std::string &output_correspondence_path_,
+	std::string &output_matched_image_path_)
+{
+	// The root directory where the "results" and "data" folder are
+	const std::string root_dir = "";
+
+	// The directory to which the results will be saved
+	std::string dir = root_dir + "results/" + scene_name_;
+
+	// Create the task directory if it doesn't exist
+	if (stat(dir.c_str(), &info) != 0) // Check if exists
+		if (_mkdir(dir.c_str()) != 0) // Create it, if not
+		{
+			fprintf(stderr, "Error while creating a new folder in 'results'\n");
+			return false;
+		}
+
+	// The source image's path
+	src_image_path_ =
+		root_dir + "data/" + scene_name_ + "/" + scene_name_ + "1.jpg";
+	if (cv::imread(src_image_path_).empty())
+		src_image_path_ = root_dir + "data/" + scene_name_ + "/" + scene_name_ + "1.png";
+	if (cv::imread(src_image_path_).empty())
+	{
+		fprintf(stderr, "Error while loading image '%s'\n", src_image_path_);
+		return false;
+	}
+
+	// The destination image's path
+	dst_image_path_ =
+		root_dir + "data/" + scene_name_ + "/" + scene_name_ + "2.jpg";
+	if (cv::imread(dst_image_path_).empty())
+		dst_image_path_ = root_dir + "data/" + scene_name_ + "/" + scene_name_ + "2.png";
+	if (cv::imread(dst_image_path_).empty())
+	{
+		fprintf(stderr, "Error while loading image '%s'\n", dst_image_path_);
+		return false;
+	}
+
+	// The path where the detected correspondences (before the robust estimation) will be saved (or loaded from if exists)
+	input_correspondence_path_ =
+		root_dir + "results/" + scene_name_ + "/" + scene_name_ + "_points_with_no_annotation.txt";
+	// The path where the inliers of the estimated fundamental matrices will be saved
+	output_correspondence_path_ =
+		root_dir + "results/" + scene_name_ + "/result_" + scene_name_ + ".txt";
+	// The path where the matched image pair will be saved
+	output_matched_image_path_ =
+		root_dir + "results/" + scene_name_ + "/matches_" + scene_name_ + ".png";
+
+	return true;
+}
+
+
+void testMultiHomographyFitting(
+	const std::string &source_path_,
+	const std::string &destination_path_,
+	const std::string &out_correspondence_path_,
+	const std::string &in_correspondence_path_,
+	const std::string &output_match_image_path_,
+	const double confidence_,
+	const double inlier_outlier_threshold_,
+	const double spatial_coherence_weight_,
+	const size_t cell_number_in_neighborhood_graph_,
+	const int fps_)
+{
+	// Read the images
+	cv::Mat source_image = cv::imread(source_path_); // The source image
+	cv::Mat destination_image = cv::imread(destination_path_); // The destination image
+
+	if (source_image.empty()) // Check if the source image is loaded successfully
+	{
+		printf("An error occured while loading image '%s'\n",
+			source_path_.c_str());
+		return;
+	}
+
+	if (destination_image.empty()) // Check if the destination image is loaded successfully
+	{
+		printf("An error occured while loading image '%s'\n",
+			destination_path_.c_str());
+		return;
+	}
+
+	// Detect or load point correspondences using AKAZE 
+	cv::Mat points;
+	detectFeatures(
+		in_correspondence_path_, // The path where the correspondences are read from or saved to.
+		source_image, // The source image
+		destination_image, // The destination image
+		points); // The detected point correspondences. Each row is of format "x1 y1 x2 y2"
+	
+	// Initialize the neighborhood used in Graph-cut RANSAC and, perhaps,
+	// in the sampler if NAPSAC or Progressive-NAPSAC sampling is applied.
+	std::chrono::time_point<std::chrono::system_clock> start, end; // Variables for time measurement
+	start = std::chrono::system_clock::now(); // The starting time of the neighborhood calculation
+	GridNeighborhoodGraph neighborhood(&points,
+		source_image.cols / static_cast<double>(cell_number_in_neighborhood_graph_),
+		source_image.rows / static_cast<double>(cell_number_in_neighborhood_graph_),
+		destination_image.cols / static_cast<double>(cell_number_in_neighborhood_graph_),
+		destination_image.rows / static_cast<double>(cell_number_in_neighborhood_graph_),
+		cell_number_in_neighborhood_graph_);
+	end = std::chrono::system_clock::now(); // The end time of the neighborhood calculation
+	std::chrono::duration<double> elapsed_seconds = end - start; // The elapsed time in seconds
+	printf("Neighborhood calculation time = %f secs\n", elapsed_seconds.count());
+
+	// Checking if the neighborhood graph is initialized successfully.
+	if (!neighborhood.isInitialized())
+	{
+		fprintf(stderr, "The neighborhood graph is not initialized successfully.\n");
+		return;
+	}
+	
+	// Initializing the samplers
+	UniformSampler main_sampler(&points); // The main sampler is used inside the local optimization
+	UniformSampler local_optimization_sampler(&points); // The local optimization sampler is used inside the local optimization
+	
+	// Applying Progressive-X
+	ProgressiveX<GridNeighborhoodGraph,
+		DefaultHomographyEstimator,
+		UniformSampler,
+		UniformSampler> progressive_x;
+
+	progressive_x.run(points,
+		neighborhood,
+		main_sampler,
+		local_optimization_sampler);
+
+	// Draw the inlier matches to the images
+	RANSACStatistics statistics;
+	statistics.inliers = progressive_x.getStatistics().inliers_of_each_model.back();
+	
+	cv::Mat match_image;
+	drawMatches(points,
+		statistics.inliers,
+		source_image,
+		destination_image,
+		match_image);
+
+	printf("Saving the matched images to file '%s'.\n", output_match_image_path_.c_str());
+	imwrite(output_match_image_path_, match_image); // Save the matched image to file
+	printf("Saving the inlier correspondences to file '%s'.\n", out_correspondence_path_.c_str());
+	savePointsToFile(points, out_correspondence_path_.c_str(), &statistics.inliers); // Save the inliers to file
+
+	printf("Press a button to continue...\n");
+
+	// Showing the image
+	showImage(match_image,
+		"Inlier correspondences",
+		1600,
+		1200,
+		true);
+}
